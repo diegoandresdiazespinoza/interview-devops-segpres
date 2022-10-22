e7a7f9e16ece38738b60b8f67f6730c9d089c3c9
Diego Diaz Espinoza <diegodiazespinoza@gmail.com>
2022-10-22 00:28:33 -0300

    [ADD] desafio 2, 3 y 4, test para ambos backend

--- api/run-api-tests.sh
+++ api/run-api-tests.sh
@@ -14,3 +14,12 @@ npx newman run $SCRIPTDIR/Conduit.postman_collection.json \
   --global-var "USERNAME=$USERNAME" \
   --global-var "EMAIL=$EMAIL" \
   --global-var "PASSWORD=$PASSWORD"
+
+APIURL=${APIURL:-http://localhost:5000}
+
+npx newman run $SCRIPTDIR/Conduit.postman_collection.json \
+  --delay-request 500 \
+  --global-var "APIURL=$APIURL" \
+  --global-var "USERNAME=$USERNAME" \
+  --global-var "EMAIL=$EMAIL" \
+  --global-var "PASSWORD=$PASSWORD"
